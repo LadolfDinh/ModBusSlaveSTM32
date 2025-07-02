477f6e8baec38a146de1a00609069fd6a989479e
lap dinh <cudinhlol@gmail.com>
2025-07-02 20:33:10 +0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/modbusSlave.elf.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/iot/modbusSlave/Core/Startup/startup_stm32f205vctx.s" "D:/iot/modbusSlave/cmake-build-debug/CMakeFiles/modbusSlave.elf.dir/Core/Startup/startup_stm32f205vctx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F205xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/iot/modbusSlave/Core/Inc"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Inc"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Inc/Legacy"
+  "D:/iot/modbusSlave/Drivers/CMSIS/Device/ST/STM32F2xx/Include"
+  "D:/iot/modbusSlave/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/iot/modbusSlave/Core/Src/main.c" "CMakeFiles/modbusSlave.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Core/Src/main.c.obj.d"
+  "D:/iot/modbusSlave/Core/Src/stm32f2xx_hal_msp.c" "CMakeFiles/modbusSlave.elf.dir/Core/Src/stm32f2xx_hal_msp.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Core/Src/stm32f2xx_hal_msp.c.obj.d"
+  "D:/iot/modbusSlave/Core/Src/stm32f2xx_it.c" "CMakeFiles/modbusSlave.elf.dir/Core/Src/stm32f2xx_it.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Core/Src/stm32f2xx_it.c.obj.d"
+  "D:/iot/modbusSlave/Core/Src/syscalls.c" "CMakeFiles/modbusSlave.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/iot/modbusSlave/Core/Src/sysmem.c" "CMakeFiles/modbusSlave.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/iot/modbusSlave/Core/Src/system_stm32f2xx.c" "CMakeFiles/modbusSlave.elf.dir/Core/Src/system_stm32f2xx.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Core/Src/system_stm32f2xx.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_cortex.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_cortex.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_cortex.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_dma.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_dma.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_dma.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_dma_ex.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_dma_ex.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_exti.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_exti.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_exti.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_flash.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_flash.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_flash.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_flash_ex.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_flash_ex.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_gpio.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_gpio.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_gpio.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_pwr.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_pwr.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_pwr.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_pwr_ex.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_pwr_ex.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_rcc.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_rcc.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_rcc.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_rcc_ex.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_rcc_ex.c.obj.d"
+  "D:/iot/modbusSlave/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_uart.c" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_uart.c.obj" "gcc" "CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_uart.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_gpio.c.obj.d
@@ -0,0 +1,35 @@
+CMakeFiles/modbusSlave.elf.dir/Drivers/STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_gpio.c.obj: \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Src\stm32f2xx_hal_gpio.c \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal.h \
+ D:\iot\modbusSlave\Core\Inc/stm32f2xx_hal_conf.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_rcc.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_def.h \
+ D:\iot\modbusSlave\Drivers\CMSIS\Device\ST\STM32F2xx\Include/stm32f2xx.h \
+ D:\iot\modbusSlave\Drivers\CMSIS\Device\ST\STM32F2xx\Include/stm32f205xx.h \
+ D:\iot\modbusSlave\Drivers\CMSIS\Include/core_cm3.h \
+ C:\ST\STM32CubeIDE_1.17.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.12.3.rel1.win32_1.1.0.202410251130\tools\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stdint.h \
+ C:\ST\STM32CubeIDE_1.17.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.12.3.rel1.win32_1.1.0.202410251130\tools\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/stdint.h \
+ C:\ST\STM32CubeIDE_1.17.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.12.3.rel1.win32_1.1.0.202410251130\tools\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/machine/_default_types.h \
+ C:\ST\STM32CubeIDE_1.17.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.12.3.rel1.win32_1.1.0.202410251130\tools\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/features.h \
+ C:\ST\STM32CubeIDE_1.17.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.12.3.rel1.win32_1.1.0.202410251130\tools\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/_newlib_version.h \
+ C:\ST\STM32CubeIDE_1.17.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.12.3.rel1.win32_1.1.0.202410251130\tools\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_intsup.h \
+ C:\ST\STM32CubeIDE_1.17.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.12.3.rel1.win32_1.1.0.202410251130\tools\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_stdint.h \
+ D:\iot\modbusSlave\Drivers\CMSIS\Include/cmsis_version.h \
+ D:\iot\modbusSlave\Drivers\CMSIS\Include/cmsis_compiler.h \
+ D:\iot\modbusSlave\Drivers\CMSIS\Include/cmsis_gcc.h \
+ D:\iot\modbusSlave\Drivers\CMSIS\Include/mpu_armv7.h \
+ D:\iot\modbusSlave\Drivers\CMSIS\Device\ST\STM32F2xx\Include/system_stm32f2xx.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ C:\ST\STM32CubeIDE_1.17.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.12.3.rel1.win32_1.1.0.202410251130\tools\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stddef.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_rcc_ex.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_gpio.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_gpio_ex.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_exti.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_dma.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_dma_ex.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_cortex.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_flash.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_flash_ex.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_pwr.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_pwr_ex.h \
+ D:\iot\modbusSlave\Drivers\STM32F2xx_HAL_Driver\Inc/stm32f2xx_hal_uart.h
